fa767a0537cc12565f458d46d3f69ad9e2e99a98
zhuhxi <hongxizhuqiling@gmail.com>
2025-06-18 00:45:14 +0800

    Add conditional Unet code

--- train.bash
+++ train.bash
@@ -1,5 +1,6 @@
 python train.py --model-name ecnet --model-class EcNet --ckpt-name EcNet --epochs 80
-python train.py --model-name eitnet --model-class EITNet --ckpt-name EITNet --epochs 1400
-python train.py --model-name sadb_net --model-class SADB_Net --ckpt-name SADB_Net --epochs 1400
-python train.py --model-name cnneim --model-class CNN_EIM --ckpt-name CNN_EIM --epochs 1400
+python train.py --model-name eitnet --model-class EITNet --ckpt-name EITNet --epochs 80
+python train.py --model-name sadb_net --model-class SADB_Net --ckpt-name SADB_Net --epochs 80
+python train.py --model-name cnneim --model-class CNN_EIM --ckpt-name CNN_EIM --epochs 80
 python train.py --model-name srcnn --model-class SRCNN --ckpt-name SRCNN --epochs 80
+python train.py --model-name cunet --model-class CUnet --ckpt-name CUnet --epochs 80
